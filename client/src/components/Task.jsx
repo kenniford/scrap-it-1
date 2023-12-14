bfe0cec478aeefe6bf60c64b1641f843e482b7dd
kenniford <kim.yb.kenny@gmail.com>
2023-12-14 15:56:44 -0600

    mvp complete

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,53 +1,80 @@
 import React, { useState } from 'react';
 
-function Task({title, description, done, onToggle, onDelete, onRename,onRenameDesc}) {
+function Task({
+  title,
+  description,
+  done,
+  onToggle,
+  onDelete,
+  onRename,
+  onRenameDesc,
+}) {
   const [editMode, setEditMode] = useState(false);
   const [editModeDesc, setEditModeDesc] = useState(false);
 
   return (
-    <div className="mb-4 w-auto">
-      <div className='flex'>
+    <div
+      className={`mb-4 w-auto ${
+        done ? 'line-through' : ''
+      } w-96 flex items-center`}>
+      <div>
         <input
           type='checkbox'
           defaultChecked={done}
           onClick={() => onToggle(!done)}
+          className='h-4 w-4 flex'
         />
       </div>
-      {/* if edit mode is false, display title */}
-      {!editMode && (
-        <div onClick={setEditMode(prev => !prev)}>
-          <h1>Title: {title}</h1>
-        </div>
-      )}
-      {/* if edit mode is true, display title */}
-      {editMode && (
-        <form className='flex'>
-          <h1 className="mr-2">Title: </h1>
-          <input
-            type='text'
-            value={title}
-            onChange={event => onRename(event.target.value)}
-          />
-        </form>
-      )}
-      {!editModeDesc && (
-        <div onClick={setEditModeDesc(prev => !prev)}>
-          <h1>Description: {description}</h1>
-        </div>
-      )}
-      {editModeDesc && (
-        <form className='flex'>
-          <h1 className="mr-2">Description: </h1>
-          <input
-            type='text'
-            value={description}
-            onChange={event => onRenameDesc(event.target.value)}
-          />
-        </form>
-      )}
-      <button className='border border-slate-800' onClick={onDelete}>
-        Delete
-      </button>
+      <div className="">
+        {/* if edit mode is false, display title */}
+        {!editMode && (
+          <div onClick={setEditMode(prev => !prev)}>
+            <h1
+              className={`${done ? 'text-gray-500 line-through' : 'text-black'} font-bold text-lg`}>
+              {title}
+            </h1>
+          </div>
+        )}
+        {/* if edit mode is true, display input*/}
+        {editMode && (
+          <form className='flex my-1'>
+            {/* <h1 className="mr-2">Title: </h1> */}
+            <input
+              type='text'
+              value={title}
+              onChange={event => onRename(event.target.value)}
+              className='w-80 bg-slate-100 font-bold text-lg'
+              placeholder='edit title'
+            />
+          </form>
+        )}
+        {!editModeDesc && (
+          <div onClick={setEditModeDesc(prev => !prev)}>
+            <h1
+              className={`${
+                done ? 'text-gray-500 line-through' : 'text-black'
+              } text-sm`}>
+              Description: {description}
+            </h1>
+          </div>
+        )}
+        {editModeDesc && (
+          <form className='flex my-1'>
+            {/* <h1 className="mr-2">Description: </h1> */}
+            <textarea
+              value={description}
+              onChange={(event) => onRenameDesc(event.target.value)}
+              className='w-80 h-auto bg-slate-100 p-2 resize-none text-sm'
+              placeholder='Add note'
+            />
+          </form>
+        )}
+      </div>
+      <div>
+        <button className='border border-slate-800 p-2 bg-red-500' onClick={onDelete}>
+          X
+        </button>
+      </div>
     </div>
   );
 }
--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -5,13 +5,19 @@ function TaskForm() {
   const { onAdd } = useContext(TaskContext);
   const [taskTitle, setTaskTitle] = useState('');
   const [taskDescription, setTaskDescription] = useState('');
+  const [taskTitleErr, setTaskTitleErr] = useState(false);
 
   const addTask = event => {
     event.preventDefault();
     // add conditional to check if tasktitle or taskdescription are not empty
-    onAdd(taskTitle, taskDescription);
-    setTaskTitle('');
-    setTaskDescription('');
+    if (!taskTitle) {
+      setTaskTitleErr(true);
+    } else {
+      onAdd(taskTitle, taskDescription);
+      setTaskTitleErr(false);
+      setTaskTitle('');
+      setTaskDescription('');
+    }
   };
 
   const clearList = e => {
@@ -19,33 +25,42 @@ function TaskForm() {
   };
 
   return (
-    <div className='pt-4 w-80'>
-      <h1 className='flex justify-center mt-16'>Kenny's To Do List</h1>
-      <form>
-        <div className='flex flex-col'>
+    <div className='flex flex-col items-center py-4 bg-slate-100 w-96'>
+      <h1 className='text-xl font-bold mt-8 mb-4'>Kenny's To Do List</h1>
+      <form className='flex flex-col items-center w-full max-w-md'>
+        <div className='flex flex-col w-full mb-4'>
           <input
             type='text'
             value={taskTitle}
             onChange={event => setTaskTitle(event.target.value)}
-            className='focus:outline-none py-2 bg-slate-100'
-            placeholder='add a new task'
+            className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none bg-white'
+            placeholder='Add a new task'
           />
           <textarea
             type='text'
             value={taskDescription}
             onChange={event => setTaskDescription(event.target.value)}
-            className='focus:outline-none py-2 bg-slate-100'
-            placeholder='add a note'
+            className='w-full px-4 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none h-28 bg-white'
+            placeholder='Add a note'
           />
         </div>
-        <div className='flex flex-col'>
+        <div>
+          <p className={`relative ${taskTitleErr ? 'text-red-600' : 'invisible'}`}>
+            Please input a task name
+          </p>
+        </div>
+        <div className='flex flex-col w-full'>
           <button
             onClick={addTask}
-            className='px-4 border border-slate-800 bg-blue-300'>
+            className='px-4 py-2 mb-2 border border-slate-800 bg-blue-300 rounded-md'>
             Add Task
           </button>
 
-          <button onClick={clearList}>Clear list</button>
+          <button
+            onClick={clearList}
+            className='px-4 py-2 border border-slate-800 rounded-md text-gray-700 mb-4'>
+            Clear list
+          </button>
         </div>
       </form>
     </div>
